6246ecce16d1ef941474281a6405bfe309348ec0
Cody Condon <codycondon50@gmail.com>
2016-02-09 11:59:56 -0500

    add secure passwrd and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true, length: {maximum: 105},
             uniqueness: {case_sensitive:false},
             format: { with: VALID_EMAIL_REGEX} #this checks for valid email format
-
+              has_secure_password
 
 end
