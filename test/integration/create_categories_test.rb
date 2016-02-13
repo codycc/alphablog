538bceb33b6e4b5b725b5c0290bdb9fd18003a58
Cody Condon <codycondon50@gmail.com>
2016-02-13 11:49:09 -0500

    Fix tests and update navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create(username: "john", email:"john@example.com", password: "password", admin:true)
+  end
+
   test "get new category form and create category" do
+      sign_in_as(@user,"password")
       get new_category_path #checking that the path works
       assert_template 'categories/new' #checking that the template renders
       assert_difference 'Category.count',1 do  #checkign if the count of categories went up, if category is made then it would
@@ -12,6 +17,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
+    sign_in_as(@user,"password")
     get new_category_path
     assert_template 'categories/new'
      assert_no_difference 'Category.count' do  #there would be no difference in count since new category wasnt created
