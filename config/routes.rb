faffc5a55cf093aad32f56c51823b1fa3f9e4bb8
Cody Condon <codycondon50@gmail.com>
2016-02-13 10:33:42 -0500

    add integration testing for categories

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ resources :articles
 get 'login', to:'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
+
+resources :categories, except: [:destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
